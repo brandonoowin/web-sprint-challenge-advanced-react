8a6506c7a31325cd29d4328a75e70367e0ea029f
Brandon Nguyen <brandonxnguyen@gmail.com>
2023-09-20 21:45:23 -0700

    Created some tests for AppFunctional.JS

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,14 @@
+import AppFunctional from "./AppFunctional"
+import React from "react" 
+import { render, screen, waitFor } from '@testing-library/react'
 // Write your tests here
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(false).toBe(false)
 })
+
+test('renders grid to DOM without Errors', () => {
+  render(
+    <AppFunctional />
+  )
+})
+
